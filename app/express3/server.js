a198e6feb5232abcf6cd65f5bab2101211d43bb7
Akuma <sergiiosercopi@gmail.com>
2020-12-17 14:27:59 +0100

    advanced in mongodb

--- app/express3/server.js
+++ app/express3/server.js
@@ -2,22 +2,38 @@ const express = require('express');
 const os = require('os');
 const app = express();
 const port = 3000;
-
+const { MongoClient } = require('mongodb');
+const uri = 'mongodb://root:example@mongo:27017';
+const connect = async uri => {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  try {
+    const connection = await client.connect();
+    return connection;
+    /*  const dbList = await client
+      .db()
+      .admin()
+      .listDatabases();
+    dbList.databases.forEach(db => console.log(db.name)); */
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.close();
+  }
+};
+const connection = connect(uri);
 app.get('/', (req, res) => {
   //res.send(os.hostname());
   //basic mongodb connection
-  const MongoClient = require('mongodb').MongoClient;
-  const url = 'mongodb://root:example@localhost:27017/prueba';
-  const response = [];
-  MongoClient.connect(url, function(err, db) {
-    console.log(db);
-    const cursor = db.collection('prueba-collection').find();
+  //db.createUser({user:"pruebauser",pwd:"1234",roles:[{role:"readWrite",db:"prueba"}]})
 
-    cursor.each(function(err, doc) {
-      response.push(doc);
-    });
-    res.send(response);
-  });
+  connection
+    .then(connection => {
+      const result = connection
+        .db('prueba')
+        .collection('prueba-collection')
+        .find();
+    })
+    .catch(error => console.error(error));
 });
 
 app.listen(port, () => {
