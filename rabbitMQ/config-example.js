7a58e3dd687122612dd6257763d5e5267911f316
sercopi <47920671+sercopi@users.noreply.github.com>
2021-08-25 16:20:00 +0200

    Update config-example.js
    example of config file, just copy your email, but remember, this email must have 3rd party uses active on the account

--- rabbitMQ/config-example.js
+++ rabbitMQ/config-example.js
@@ -1,10 +1,11 @@
 const config = {
   SERVICE: 'gmail',
-  EMAIL: 'your gmail address',
-  PASS: 'your password',
-  QUEUE_EMAIL: 'email',
-  QUEUE_KEY: 'key',
-  QUEUE_KEY_BACK: 'keyBack',
+  EMAIL: '',
+  PASS: '',
+  QUEUE_VERIFY_EMAIL: 'verify-email',
+  QUEUE_FORGOT_PASSWORD: 'forgot-password',
+  CLIENT_ADDRESS: 'http://localhost:8080/#',
+  CLIENT_VERIFY_EMAIL_ENDPOINT: '/verifyEmail',
+  CLIENT_FORGOT_PASSWORD_ENDPOINT: '/restorePassword',
 };
 module.exports = Object.freeze({ ...config });
-//rename this to config, and set it with your Email and Password for the email sender
